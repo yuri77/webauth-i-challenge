41abdf6322614280089d19301ac4d50911524c4e
yurika sakae <yurika.sun@gmail.com>
2019-09-16 14:30:39 -0700

    server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const server = express;
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("API is up");
+});
+
+module.exports = server;
